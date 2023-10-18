8aa97c1391f2fd2d2b352d64a67f30df1fcc90ec
hab <hab@luftborn.com>
2023-10-18 17:03:29 +0200

    configure producer

--- producer/producer.js
+++ producer/producer.js
@@ -1,25 +1,26 @@
 const amqp = require('amqplib');
-require('dotenv').config()
+require('dotenv').config();
 
 async function main() {
   try {
-    const username = process.env.RABBITMQ_USERNAME; // Use the environment variable for the username
+    const username = process.env.RABBITMQ_USERNAME;
     const password = process.env.RABBITMQ_PASSWORD;
     const connection = await amqp.connect(`amqp://${username}:${password}@rabbitmq-service`);
     
-
     const queue = 'producer-queue';
-    const message = 'Hello, RabbitMQ! Here\'s a message';
-
-    // Assert the queue (create it if it doesn't exist)
+    
     const channel = await connection.createChannel();
     await channel.assertQueue(queue);
 
-    // Send a message to the queue
-    channel.sendToQueue(queue, Buffer.from(message));
-    console.log(`Sent: ${message}`);
+    // Function to send a message to the queue
+    const sendMessage = async () => {
+      const message = `Hello, RabbitMQ! Here's a message at ${(new Date()).toDateString()}.`;
+      channel.sendToQueue(queue, Buffer.from(message));
+      console.log(`Sent: ${message}`);
+    };
 
-    // Consume messages from the queue
+    setInterval(sendMessage, (process.env.MESSAGE_INTERVALS * 1000) || 10000); // 10,000 milliseconds (10 seconds)
+    
   } catch (error) {
     console.error('Error:', error.message);
   }
